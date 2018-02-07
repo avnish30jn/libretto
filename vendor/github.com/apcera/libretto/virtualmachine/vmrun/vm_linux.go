7acd1958a3db1b8dd84aedaf1410a28066120560
nirav1007 <nirav@apporbit.com>
2018-02-07 05:50:28 -0500

    MRPHS-4921: Vsphere build was failing. Removed incorrectly vendored libretto

--- vendor/github.com/apcera/libretto/virtualmachine/vmrun/vm_linux.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2015 Apcera Inc. All rights reserved.
-
-// +build linux
-
-package vmrun
-
-// VMRunPath is a hardcoded path to fall back to when vmrun is not in the $PATH.
-var VMRunPath = "/usr/bin/vmrun"
